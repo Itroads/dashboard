c8c0063d55a3807bc3170e2f2992cdd49b648ecc
Joker <740199246@qq.com>
2017-07-20 08:53:03 +0800

    备份

---
+++ iconfont/iconfont.js
@@ -0,0 +1,128 @@
+;(function(window) {
+
+  var svgSprite = '<svg>' +
+    '' +
+    '<symbol id="icon-xinicon02" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M800 64l-576 0C204.8 64 192 76.8 192 96l0 832C192 947.2 204.8 960 224 960l576 0c19.2 0 32-12.8 32-32l0-832C832 76.8 819.2 64 800 64zM544 608C524.8 608 512 595.2 512 576s12.8-32 32-32S576 556.8 576 576 563.2 608 544 608zM704 640c-38.4 0-64-25.6-64-64s25.6-64 64-64 64 25.6 64 64S742.4 640 704 640zM768 448 256 448 256 320l512 0L768 448zM768 256 256 256 256 128l512 0L768 256z"  ></path>' +
+    '' +
+    '<path d="M704 576m-32 0a0.5 0.5 0 1 0 64 0 0.5 0.5 0 1 0-64 0Z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '</svg>'
+  var script = function() {
+    var scripts = document.getElementsByTagName('script')
+    return scripts[scripts.length - 1]
+  }()
+  var shouldInjectCss = script.getAttribute("data-injectcss")
+
+  /**
+   * document ready
+   */
+  var ready = function(fn) {
+    if (document.addEventListener) {
+      if (~["complete", "loaded", "interactive"].indexOf(document.readyState)) {
+        setTimeout(fn, 0)
+      } else {
+        var loadFn = function() {
+          document.removeEventListener("DOMContentLoaded", loadFn, false)
+          fn()
+        }
+        document.addEventListener("DOMContentLoaded", loadFn, false)
+      }
+    } else if (document.attachEvent) {
+      IEContentLoaded(window, fn)
+    }
+
+    function IEContentLoaded(w, fn) {
+      var d = w.document,
+        done = false,
+        // only fire once
+        init = function() {
+          if (!done) {
+            done = true
+            fn()
+          }
+        }
+        // polling for no errors
+      var polling = function() {
+        try {
+          // throws errors until after ondocumentready
+          d.documentElement.doScroll('left')
+        } catch (e) {
+          setTimeout(polling, 50)
+          return
+        }
+        // no errors, fire
+
+        init()
+      };
+
+      polling()
+        // trying to always fire before onload
+      d.onreadystatechange = function() {
+        if (d.readyState == 'complete') {
+          d.onreadystatechange = null
+          init()
+        }
+      }
+    }
+  }
+
+  /**
+   * Insert el before target
+   *
+   * @param {Element} el
+   * @param {Element} target
+   */
+
+  var before = function(el, target) {
+    target.parentNode.insertBefore(el, target)
+  }
+
+  /**
+   * Prepend el to target
+   *
+   * @param {Element} el
+   * @param {Element} target
+   */
+
+  var prepend = function(el, target) {
+    if (target.firstChild) {
+      before(el, target.firstChild)
+    } else {
+      target.appendChild(el)
+    }
+  }
+
+  function appendSvg() {
+    var div, svg
+
+    div = document.createElement('div')
+    div.innerHTML = svgSprite
+    svgSprite = null
+    svg = div.getElementsByTagName('svg')[0]
+    if (svg) {
+      svg.setAttribute('aria-hidden', 'true')
+      svg.style.position = 'absolute'
+      svg.style.width = 0
+      svg.style.height = 0
+      svg.style.overflow = 'hidden'
+      prepend(svg, document.body)
+    }
+  }
+
+  if (shouldInjectCss && !window.__iconfont__svg__cssinject__) {
+    window.__iconfont__svg__cssinject__ = true
+    try {
+      document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>");
+    } catch (e) {
+      console && console.log(e)
+    }
+  }
+
+  ready(appendSvg)
+
+
+})(window)
